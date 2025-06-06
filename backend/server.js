42bdaf2fd6d66956c86a328d438f0ee5689a9356
Michele Campanello <Campanellomichele@outlook.it>
2025-06-06 23:54:47 +0200

    rate-limiter rimosso

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-import morgan from "morgan"; // --> 1. Aggiunto per il logging delle richieste
-import rateLimit from "express-rate-limit"; // --> 2. Aggiunto per la sicurezza (rate limiting)
-import hpp from "hpp"; // --> 3. Aggiunto per la sicurezza (parameter pollution)
-import { verifyOrder } from "./controllers/order.controller.js";
+import morgan from "morgan";
+import hpp from "hpp";
 
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
@@ -25,27 +23,11 @@ app.set("trust proxy", 1);
 connectDB();
 connectCloudinary();
 
-app.post(
-	"/api/order/verify",
-	express.raw({ type: "application/json" }),
-	verifyOrder
-);
-
 // --- Middleware di Sicurezza e Logging ---
 app.use(helmet()); // Imposta vari header HTTP per la sicurezza
 app.use(express.json({ limit: "10kb" })); // Limita la dimensione del payload JSON
 app.use(cookieParser());
 app.use(hpp()); // Protegge contro l'inquinamento dei parametri HTTP
-
-// Rate Limiting per prevenire attacchi brute-force
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minuti
-	max: 100, // Limita ogni IP a 100 richieste per finestra
-	standardHeaders: true,
-	legacyHeaders: false,
-	message: "Troppe richieste da questo IP, riprova tra 15 minuti.",
-});
-app.use("/api", limiter); // Applica il rate limiter a tutte le rotte API
 
 // Logging delle richieste in modalità sviluppo
 if (process.env.NODE_ENV === "development") {
